f1bd6de86c83d8b952f14cca9a44dbdb085b4b50
SoSo12121212 <yo789yo25@gmail.com>
2025-02-28 10:33:59 +0900

    ui: 포트폴리오 페이지 버튼 컴포넌트 추가

---
+++ src/components/common/portfolioPages/PortButton.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: rgba(178, 196, 223, 0.2);
+
+  padding: 20px 10px;
+  border-radius: 10px;
+  border: none;
+  margin: 0 10px;
+  width: 140px;
+  height: 50px;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  margin-top: 40px;
+`;
+
+const PortButtons = () => {
+  return (
+    <ButtonContainer>
+      <Button>저장하기</Button>
+      <Button>삭제하기</Button>
+    </ButtonContainer>
+  );
+};
+
+export default PortButtons;
