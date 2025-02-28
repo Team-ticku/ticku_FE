0f29115e5cfa24b191190153022935f053bdec97
sonnnnhe <fladi39@naver.com>
2025-02-28 16:48:43 +0900

    ui: 티코 페이지 추가

---
+++ src/pages/tickoPages/TickoPage.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import ButtonMenu from "../../components/tickoPages/tickoPage/ButtonMenu";
+import CharIntroduce from "../../components/tickoPages/tickoPage/CharIntroduce";
+
+export default function tickoPage() {
+  return (
+    <>
+      <CharIntroduce></CharIntroduce>
+      <ButtonMenu></ButtonMenu>
+    </>
+  );
+}
