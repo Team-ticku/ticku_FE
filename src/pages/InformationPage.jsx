26f38fe349370aafd35fc86623944ebf5bd9d2f6
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-02-28 16:37:46 +0900

    chore: 충돌 버그 수정

--- src/pages/InformationPage.jsx
+++ src/pages/InformationPage.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-// import reactLogo from "./assets/react.svg";
-import PortTitle from "../../components/portfolioPages/PortTitle";
-import PortBox from "../../components/portfolioPages/PortBox";
-import PortButton from "../../components/portfolioPages/PortButton";
-// import "../App.css";
 import styled from "styled-components";
+import Navigation from "../components/information/Navigation";
+import InfoFirst from "../components/information/InfoFirst";
+import { Routes, Route } from "react-router-dom";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-function PortfolioMainPage() {
+function Information() {
   return (
-    <Container>
-      <PortTitle />
-      <PortBox />
-      <PortButton />
-    </Container>
+    <div>
+      <Navigation />
+
+      <Routes>
+        <Route path="/chart" element={<div>차트 페이지</div>} />
+        <Route path="/finance" element={<div>기업 재무 페이지</div>} />
+        <Route path="/volume" element={<div>거래량 페이지</div>} />
+        <Route path="/news" element={<div>뉴스 페이지</div>} />
+        <Route path="/dividend" element={<div>배당 페이지</div>} />
+        <Route path="/result" element={<div>실적 페이지</div>} />
+        <Route path="/" element={<InfoFirst />} />{" "}
+        <Route path="*" element={<div>404 Not Found</div>} />{" "}
+      </Routes>
+    </div>
   );
 }
 
-export default PortfolioMainPage;
+export default Information;
