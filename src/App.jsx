b46c4f00719f89472ba796546f927970c9113280
jjeunv <124152742+jjeunv@users.noreply.github.com>
2025-02-28 16:33:06 +0900

    fix: 충돌 버그 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { Routes, Route } from "react-router-dom";
 
 // import CalendarPage from "./pages/CalendarPage";
 // import CommunityPostPage from "./pages/CommunityPostsPage";
-import InformationPage from "./pages/InformationPage";
-// import MainPage from "./pages/mainpage/MainPage";
+// import InformationPage from "./pages/InformationPage";
+import MainPage from "./pages/mainpage/MainPage";
 // import MyPage from "./pages/MyPage";
-// import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
+import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
 // import TickoPage from "./pages/TickoPage";
+
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<MainPage />}></Route> */}
-        {/* <Route path="/portmn" element={<PortfolioMainPage />}></Route> */}
-        <Route path="/information" element={<InformationPage />}></Route>
-        {/* 
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/portmn" element={<PortfolioMainPage />}></Route>
+        {/* <Route path="/information" element={<InformationPage />}></Route>
+
         <Route path="/communityposts" element={<CommunityPostPage />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/calen" element={<CalendarPage />}></Route>
