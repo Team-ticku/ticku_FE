75ff27a5cd2aa9cc1662c039208606956389c9e3
zangwoo Jeon <create7827@naver.com>
2025-02-28 16:17:36 +0900

    fix : 정보제공페이지 버그 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import AppInformation from "./AppInformation.jsx";
+import App from "./App.jsx";
+import BottomNavBar from "./components/common/bottomNavBar/BottomNavBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppInformation />
+    <BrowserRouter>
+      <App />
+      <BottomNavBar />
+    </BrowserRouter>
   </StrictMode>
 );
