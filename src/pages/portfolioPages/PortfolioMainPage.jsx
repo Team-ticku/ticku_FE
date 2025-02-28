e1ac2779e395af8971d910515b12a95f43e3fb28
SoSo12121212 <yo789yo25@gmail.com>
2025-02-28 10:39:49 +0900

    ui: 포트폴리오 페이지 추가

---
+++ src/pages/portfolioPages/PortfolioMainPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+// import reactLogo from "./assets/react.svg";
+import PortTitle from "../../components/common/portfolioPages/PortTitle";
+import PortBox from "../../components/common/portfolioPages/PortBox";
+import PortButton from "../../components/common/portfolioPages/PortButton";
+import "../App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+function Port() {
+  return (
+    <Container>
+      <PortTitle />
+      <PortBox />
+      <PortButton />
+    </Container>
+  );
+}
+
+export default Port;
