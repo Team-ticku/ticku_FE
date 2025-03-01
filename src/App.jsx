9cbff4018f68066baf1c31be1f2b3a4347c55398
jjeunv <jjeunv@gmail.com>
2025-03-01 21:19:43 +0900

    chore: 티코페이지 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/mainpage/MainPage";
 // import MyPage from "./pages/MyPage";
 import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
-// import TickoPage from "./pages/TickoPage";
+import TickoPage from "./pages/tickoPages/TickoPage";
 
 function App() {
   return (
@@ -14,12 +14,13 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/portmn" element={<PortfolioMainPage />}></Route>
+        <Route path="/ticko" element={<TickoPage />}></Route>
         {/* <Route path="/information" element={<InformationPage />}></Route>
 
         <Route path="/communityposts" element={<CommunityPostPage />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/calen" element={<CalendarPage />}></Route>
-        <Route path="/ticko" element={<TickoPage />}></Route> */}
+       */}
       </Routes>
     </>
   );
