e0f6322d34e3ce917fe27c1ca2a4e52d21f1740f
sonnnnhe <fladi39@naver.com>
2025-03-02 20:19:58 +0900

    refactor: 코드 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import CalendarPage from "./pages/CalendarPage";
-import CommunityPostPage from "./pages/CommunityPostsPage";
-import InformationPage from "./pages/InformationPage";
-import MainPage from "./pages/MainPage";
-import MyPage from "./pages/MyPage";
-import PortfolioMainPage from "./pages/PortfolioMainPage";
-import TickoPage from "./pages/TickoPage";
+// import CalendarPage from "./pages/CalendarPage";
+// import CommunityPostPage from "./pages/CommunityPostsPage";
+// import InformationPage from "./pages/InformationPage";
+import MainPage from "./pages/mainpage/MainPage";
+// import MyPage from "./pages/MyPage";
+import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
+// import TickoPage from "./pages/TickoPage";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/information" element={<InformationPage />}></Route>
-        <Route path="/portmn" element={<PortfolioMainPage />}></Route>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/portmn" element={<PortfolioMainPage />}></Route>
+          {/* <Route path="/information" element={<InformationPage />}></Route>
+
         <Route path="/communityposts" element={<CommunityPostPage />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/calen" element={<CalendarPage />}></Route>
-        <Route path="/ticko" element={<TickoPage />}></Route>
-      </Routes>
+        <Route path="/ticko" element={<TickoPage />}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import BottomNavBar from "./components/common/bottomNavBar/BottomNavBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <BottomNavBar />
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
