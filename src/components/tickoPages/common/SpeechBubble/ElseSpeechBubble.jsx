80a49e995ba26b38408394d21562f61bc9820b17
sonnnnhe <fladi39@naver.com>
2025-03-04 13:18:34 +0900

    ui: 말풍선 컴포넌트 추가

---
+++ src/components/tickoPages/common/SpeechBubble/ElseSpeechBubble.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import SpeechBubble from "./SpeechBubble";
+
+const BubbleWrap = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+const Dummy = styled.div`
+  display: hidden;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  flex-shrink: 0;
+`;
+
+export default function ElseSpeechBubble({ context }) {
+  return (
+    <BubbleWrap>
+      <Dummy></Dummy>
+      <SpeechBubble context={context}></SpeechBubble>
+    </BubbleWrap>
+  );
+}
---
+++ src/components/tickoPages/common/SpeechBubble/SpeechBubble.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const SpeechWrap = styled.div`
+  border: 2px solid #b2c4df;
+  border-radius: 0 15px 15px 15px;
+  padding: 11px 15px;
+`;
+
+const StyledContext = styled.div`
+  font-size: 16px;
+`;
+
+export default function SpeechBubble({ context }) {
+  return (
+    <SpeechWrap>
+      <StyledContext>{context}</StyledContext>
+    </SpeechWrap>
+  );
+}
