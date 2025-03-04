28d0f13018b666a68c687b49abc383e69c29b854
sonnnnhe <fladi39@naver.com>
2025-03-04 17:24:35 +0900

    ui: 차트 설명 페이지 추가

---
+++ src/pages/tickoPages/BasicChartPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/tickoPages/common/header/Header";
+import ChartBody from "../../components/tickoPages/BasicChartPage/ChartBody";
+
+export default function BasicChartPage() {
+  return (
+    <>
+      <Header></Header>
+      <ChartBody></ChartBody>
+    </>
+  );
+}
