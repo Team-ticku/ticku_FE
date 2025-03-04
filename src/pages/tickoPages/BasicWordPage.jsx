a0d51f1313509171a010a5c8455fe51cd7da2d71
sonnnnhe <fladi39@naver.com>
2025-03-04 13:15:21 +0900

    ui: 기초 지식 설명 페이지 추가

---
+++ src/pages/tickoPages/BasicWordPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/tickoPages/common/header/Header";
+import WordBody from "../../components/tickoPages/BasicWordPage/WordBody";
+
+export default function BasicWordPage() {
+  return (
+    <>
+      <Header></Header>
+      <WordBody></WordBody>
+    </>
+  );
+}
