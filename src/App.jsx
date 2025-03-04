629178c29c8265716ec6e5ca424ba01374d1a8b7
yeongchul <revan2015@naver.com>
2025-03-04 09:54:12 +0900

    refactor: mypage 주석 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import CommunityPostPage from "./pages/CommunityPostsPage";
 // import InformationPage from "./pages/InformationPage";
 import MainPage from "./pages/mainpage/MainPage";
-// import MyPage from "./pages/MyPage";
+import MyPage from "./pages/MyPages/MyPage";
 import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
 import TickoPage from "./pages/tickoPages/TickoPage";
 
@@ -16,10 +16,10 @@ function App() {
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/portmn" element={<PortfolioMainPage />}></Route>
           {/* <Route path="/information" element={<InformationPage />}></Route>
-        <Route path="/communityposts" element={<CommunityPostPage />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-        <Route path="/calen" element={<CalendarPage />}></Route>
-        <Route path="/ticko" element={<TickoPage />}></Route> */}
+        <Route path="/communityposts" element={<CommunityPostPage />}></Route>*/}
+          <Route path="/mypage" element={<MyPage />}></Route>
+          {/* <Route path="/calen" element={<CalendarPage />}></Route> */}
+          {/* <Route path="/ticko" element={<TickoPage />}></Route>  */}
         </Routes>
       </BrowserRouter>
     </>
