1357f830bc02f63d048a959997fbf69a8b17ce90
sonnnnhe <fladi39@naver.com>
2025-03-05 10:36:52 +0900

    ui: 계좌 설명 페이지 추가

---
+++ src/pages/tickoPages/BasicAccountPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/tickoPages/common/header/Header";
+import AccountBody from "../../components/tickoPages/basicAccountPage/AccountBody";
+
+export default function BasicAccountPage() {
+  return (
+    <>
+      <Header link="/ticko"></Header>
+      <AccountBody></AccountBody>
+    </>
+  );
+}
