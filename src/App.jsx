68c55de48ac8c1f321520cd2a2fe30693a215aaa
sonnnnhe <fladi39@naver.com>
2025-03-05 14:06:17 +0900

    chore: 라우팅 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
-// import CalendarPage from "./pages/CalendarPage";
-// import CommunityPostPage from "./pages/CommunityPostsPage";
-import InformationPage from "./pages/InformationPages/InformationPage";
-import MainPage from "./pages/mainpage/MainPage";
-import MyPage from "./pages/myPages/MyPage";
-import PortfolioMainPage from "./pages/portfolioPages/PortfolioMainPage";
-import TickoPage from "./pages/tickoPages/TickoPage";
-
-import ScrapNews from "./pages/myPages/ScrapNews";
+import TotalRoutes from "./routes/totalRoutes";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/portmn" element={<PortfolioMainPage />}></Route>
-          <Route path="/information/*" element={<InformationPage />}></Route>
-          {/* <Route path="/communityposts" element={<CommunityPostPage />}></Route> */}
-          <Route path="/mypage" element={<MyPage />}></Route>
-          {/* <Route path="/calen" element={<CalendarPage />}></Route> */}
-          {/* <Route path="/ticko" element={<TickoPage />}></Route> */}
-
-          <Route path="/mypage/scrapnews" element={<ScrapNews />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <TotalRoutes />;
 }
 
 export default App;
