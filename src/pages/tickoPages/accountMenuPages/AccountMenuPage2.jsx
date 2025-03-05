cc0790af2e14319a8af8b9da7db12cc4322645ab
sonnnnhe <fladi39@naver.com>
2025-03-05 14:05:17 +0900

    chore: 파일 경로 변경

--- src/pages/tickoPages/accountMenuPages/AccountMenuPage2.jsx
+++ src/pages/tickoPages/accountMenuPages/AccountMenuPage2.jsx
@@ -1,7 +1,7 @@
 import Header from "../../../components/tickoPages/common/header/Header";
 import AccountBody2 from "../../../components/tickoPages/basicAccountPage/menu/AccountBody2";
 
-export default function AccountPage2() {
+export default function AccountMenuPage2() {
   return (
     <>
       <Header link="/ticko/account"></Header>
