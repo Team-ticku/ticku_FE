f8731a0bddf7543e4482401c04ee63cc66d1ce00
sonnnnhe <fladi39@naver.com>
2025-03-05 14:06:07 +0900

    chore: 파일 경로 변경

---
+++ src/routes/BottomNavBarRoutes.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+// import CalendarPage from "../pages/CalendarPage";
+// import CommunityPostPage from "../pages/CommunityPostsPage";
+import InformationPage from "../pages/InformationPages/InformationPage";
+import MainPage from "../pages/mainpage/MainPage";
+import MyPage from "../pages/myPages/MyPage";
+import PortfolioMainPage from "../pages/portfolioPages/PortfolioMainPage";
+import TickoPage from "../pages/tickoPages/TickoPage";
+
+// import ScrapNews from "./pages/myPages/ScrapNews";
+
+export default function BottomNavBarRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/portmn" element={<PortfolioMainPage />}></Route>
+      <Route path="/information/*" element={<InformationPage />}></Route>
+      {/* <Route path="/communityposts" element={<CommunityPostPage />}></Route> */}
+      <Route path="/mypage" element={<MyPage />}></Route>
+      {/* <Route path="/calen" element={<CalendarPage />}></Route> */}
+      <Route path="/ticko" element={<TickoPage />}></Route>
+
+      {/* <Route path="/mypage/scrapnews" element={<ScrapNews />}></Route> */}
+    </Routes>
+  );
+}
