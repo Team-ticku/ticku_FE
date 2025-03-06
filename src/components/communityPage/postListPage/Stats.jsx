13fc8e1c56747bffaeb956b0a2c71a40b4425af2
jjeunv <jjeunv@gmail.com>
2025-03-06 09:34:49 +0900

    ui: 커뮤니티 페이지 좋아요 및 댓글 수 컴포넌트 추가

---
+++ src/components/communityPage/postListPage/Stats.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 160px;
+  box-sizing: border-box;
+  width: 100px;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Picture = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 0;
+`;
+
+const CountText = styled.p`
+  margin-left: 4px;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 30px;
+`;
+
+function Stats(props) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikeClick = (event) => {
+    event.stopPropagation();
+    setIsLiked(!isLiked);
+  };
+  return (
+    <Div>
+      <Container>
+        <Picture
+          src={
+            isLiked
+              ? "../../../public/images/heart.png"
+              : "../../../public/images/heartline.png"
+          }
+          onClick={handleLikeClick}
+        />
+        <CountText>{props.likesCount}</CountText>
+      </Container>
+      <Container>
+        <Picture src="../../../public/images/comment.png" />
+        <CountText>{props.commentsCount}</CountText>
+      </Container>
+    </Div>
+  );
+}
+
+Stats.propTypes = {
+  likesCount: PropTypes.number.isRequired,
+  commentsCount: PropTypes.number.isRequired,
+};
+
+export default Stats;
