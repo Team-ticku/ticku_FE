b5d183001ee4fd2b88d820ee94b64710d02f5e14
jjeunv <jjeunv@gmail.com>
2025-03-06 09:35:01 +0900

    ui: 커뮤니티 페이지 태그 컴포넌트 추가

---
+++ src/components/communityPage/postListPage/Tags.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Div = styled.div`
+  margin-top: 16px;
+  margin-bottom: 4px;
+  display: flex;
+  padding: 0 26px;
+`;
+
+const Tag = styled.div`
+  display: inline-flex;
+  border-radius: 7px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => getTagColor(props.tag)};
+`;
+
+const TagText = styled.p`
+  padding: 3px 10px;
+  padding-top: 4px;
+  font-size: 18px;
+  font-weight: 400;
+  color: white;
+`;
+
+function getTagColor(tag) {
+  switch (tag) {
+    case "일상":
+      return "#FDA9AB";
+    case "질문":
+      return "#DAADDA";
+    case "정보/분석":
+      return "#97D09D";
+    case "포트폴리오":
+      return "#FFD885";
+    case "종목 추천":
+      return "#B2DADF";
+    default:
+      return "#607D8B";
+  }
+}
+
+function Tags(props) {
+  return (
+    <Div>
+      <Tag tag={props.tag}>
+        <TagText>{props.tag}</TagText>
+      </Tag>
+    </Div>
+  );
+}
+
+Tags.propTypes = {
+  tag: PropTypes.string.isRequired,
+};
+
+export default Tags;
