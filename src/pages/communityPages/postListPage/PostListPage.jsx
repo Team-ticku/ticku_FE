ff0301f03ce6b97c5b574eae6125d5ce515ba2ea
jjeunv <jjeunv@gmail.com>
2025-03-06 09:36:34 +0900

    feat: 커뮤니티 페이지 글 목록 페이지 추가

---
+++ src/pages/communityPages/postListPage/PostListPage.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import PostList from "./PostList";
+import data from "../data.json";
+import WriteBtn from "../../../components/communityPage/postListPage/WriteBtn";
+import BottomNavBar from "../../../components/common/bottomNavBars/BottomNavBar";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  height: 100%;
+  position: relative;
+`;
+
+const Box = styled.div`
+  width: 100%;
+`;
+
+const TimeLine = styled.p`
+  margin: 17px 10px;
+  font-size: 30px;
+  font-weight: 750;
+  padding-left: 15px;
+`;
+
+function PostListPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <BottomNavBar />
+      <Div>
+        <TimeLine>타임라인</TimeLine>
+        <Box>
+          <PostList
+            posts={data}
+            onClickItem={(item) => {
+              navigate(`post/${item.id}`);
+            }}
+          />
+        </Box>
+        <WriteBtn
+          onClickBtn={() => {
+            navigate("/communitywrite");
+          }}
+        />
+      </Div>
+    </>
+  );
+}
+
+export default PostListPage;
