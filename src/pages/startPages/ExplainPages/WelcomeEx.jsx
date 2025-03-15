bf42b8719bc7e8693e579bf18b79d25ebb3c1968
sonnnnhe <fladi39@naver.com>
2025-03-15 19:36:11 +0900

    ui: 로고 추가

--- src/pages/startPages/ExplainPages/WelcomeEx.jsx
+++ src/pages/startPages/ExplainPages/WelcomeEx.jsx
@@ -5,23 +5,19 @@ const Wrap = styled.div`
   width: 390px;
   height: 792px;
   justify-content: center;
-  padding-top: 170px;
+  padding-top: 140px;
   box-sizing: border-box;
 `;
 
-const Logo = styled.div`
-  border-radius: 50%;
-  background-color: #b2b2b2;
-  width: 200px;
-  height: 200px;
-  text-align: center;
-  line-height: 200px;
+const Logo = styled.img`
+  width: 230px;
+  height: 230px;
 `;
 
 export default function WelcomeEx() {
   return (
     <Wrap>
-      <Logo>로고</Logo>
+      <Logo src="/images/logo_pic.png"></Logo>
     </Wrap>
   );
 }
