67c34c1cb05cddabb5d71deacd633c98c2a66581
jjeunv <jjeunv@gmail.com>
2025-03-15 20:36:38 +0900

    feat: UserProfile 컴포넌트 수정

--- src/components/common/UserProfile.jsx
+++ src/components/common/UserProfile.jsx
@@ -23,38 +23,43 @@ const UserName = styled.p`
   white-space: nowrap;
 `;
 
-function UserProfile({ height, width, fontsize, padding }) {
+function UserProfile({ height, width, fontsize, padding, isAnonymous }) {
   const [userName, setUserName] = useState("익명");
   const [userImage, setUserImage] = useState(
     "/public/images/profile_picture.png"
   );
 
   useEffect(() => {
-    const userId = localStorage.getItem("userId"); // localStorage에서 userId 가져오기
+    if (!isAnonymous) {
+      // isAnonymous가 false일 경우에만 사용자 정보를 가져오기
+      const userId = localStorage.getItem("userId"); // localStorage에서 userId 가져오기
 
-    const fetchUserProfile = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:5000/user/info/${userId}`
-        );
-        const data = await response.json();
+      const fetchUserProfile = async () => {
+        try {
+          const response = await fetch(
+            `http://localhost:5000/user/info/${userId}`
+          );
+          const data = await response.json();
 
-        setUserName(data.name || "익명"); // name이 없으면 익명으로 설정
+          setUserName(data.name || "익명"); // name이 없으면 익명으로 설정
 
-        // 서버에서 받은 이미지 경로가 상대경로라면 절대경로로 변경
-        const imageUrl = data.image
-          ? `http://localhost:5000${data.image}`
-          : "/public/images/profile_picture.png";
-        setUserImage(imageUrl);
-      } catch (err) {
-        console.error("사용자 정보를 가져오는 데 실패했습니다.", err);
+          // 서버에서 받은 이미지 경로가 상대경로라면 절대경로로 변경
+          const imageUrl = data.image
+            ? `http://localhost:5000${data.image}`
+            : "/public/images/profile_picture.png";
+          setUserImage(imageUrl);
+        } catch (err) {
+          console.error("사용자 정보를 가져오는 데 실패했습니다.", err);
+        }
+      };
+
+      if (userId) {
+        fetchUserProfile(); // userId가 있으면 사용자 정보를 가져오기
       }
-    };
-
-    if (userId) {
-      fetchUserProfile(); // userId가 있으면 사용자 정보를 가져오기
+    } else {
+      setUserName("익명"); // 익명이면 이름을 "익명"으로 설정
     }
-  }, []); // 컴포넌트 마운트 시 한 번만 실행
+  }, [isAnonymous]); // isAnonymous 값이 변경되면 재실행
 
   return (
     <Div>
