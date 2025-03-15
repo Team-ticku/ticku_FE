7fa5212a549ad1249a831b7bf4357131d8af6451
jjeunv <jjeunv@gmail.com>
2025-03-16 07:46:50 +0900

    fix: UserProfile 컴포넌트 api 연결 수정

--- src/components/common/UserProfile.jsx
+++ src/components/common/UserProfile.jsx
@@ -23,29 +23,34 @@ const UserName = styled.p`
   white-space: nowrap;
 `;
 
-function UserProfile({ height, width, fontsize, padding, isAnonymous }) {
+function UserProfile({
+  height,
+  width,
+  fontsize,
+  padding,
+  isAnonymous,
+  userId,
+  isMypage,
+}) {
   const [userName, setUserName] = useState("익명");
   const [userImage, setUserImage] = useState(
     "/public/images/profile_picture.png"
   );
 
   useEffect(() => {
-    if (!isAnonymous) {
-      // isAnonymous가 false일 경우에만 사용자 정보를 가져오기
-      const userId = localStorage.getItem("userId"); // localStorage에서 userId 가져오기
+    if (isMypage) {
+      const Id = localStorage.getItem("userId"); // localStorage에서 userId 가져오기
 
       const fetchUserProfile = async () => {
         try {
-          const response = await fetch(
-            `http://localhost:5000/user/info/${userId}`
-          );
+          const response = await fetch(`http://localhost:5000/user/info/${Id}`);
           const data = await response.json();
 
           setUserName(data.name || "익명"); // name이 없으면 익명으로 설정
 
           // 서버에서 받은 이미지 경로가 상대경로라면 절대경로로 변경
           const imageUrl = data.image
-            ? `http://localhost:5000${data.image}`
+            ? data.image
             : "/public/images/profile_picture.png";
           setUserImage(imageUrl);
         } catch (err) {
@@ -53,13 +58,32 @@ function UserProfile({ height, width, fontsize, padding, isAnonymous }) {
         }
       };
 
-      if (userId) {
-        fetchUserProfile(); // userId가 있으면 사용자 정보를 가져오기
+      if (Id) {
+        fetchUserProfile();
       }
-    } else {
-      setUserName("익명"); // 익명이면 이름을 "익명"으로 설정
+    } else if (!isAnonymous) {
+      console.log(userId._id);
+      const fetchUserProfile = async () => {
+        try {
+          const response = await fetch(
+            `http://localhost:5000/user/info/${userId._id}`
+          );
+          const data = await response.json();
+
+          setUserName(data.name || "익명"); // name이 없으면 익명으로 설정
+
+          console.log(data.image);
+          const imageUrl = data.image
+            ? data.image
+            : "/public/images/profile_picture.png";
+          setUserImage(imageUrl);
+        } catch (err) {
+          console.error("사용자 정보를 가져오는 데 실패했습니다.", err);
+        }
+      };
+      fetchUserProfile();
     }
-  }, [isAnonymous]); // isAnonymous 값이 변경되면 재실행
+  }, [isAnonymous, isMypage, userId]);
 
   return (
     <Div>
