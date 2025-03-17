f5bebfa08afd567e9ce743930afa22a02ffe3123
jjeunv <jjeunv@gmail.com>
2025-03-17 20:37:23 +0900

    chore: 경로 변경

--- src/components/communityPage/postViewPage/CommentList.jsx
+++ src/components/communityPage/postViewPage/CommentList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import CommentListItem from "./CommentListItem";
 import PropTypes from "prop-types";
+
+import CommentListItem from "./CommentListItem";
 
 /*
 
